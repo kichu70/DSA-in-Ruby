1a6522f23b5f6b845ed79a1d9d204c3a0993879d
kichu70 <kiichu7034@gmail.com>
2026-04-28 19:46:56 +0530

    first commit

--- 1.rb
+++ 1.rb
@@ -1,26 +1,31 @@
+def reverse(a)
+    a=a.to_s.reverse.to_i
+    p a
 
+end
+reverse(123)
 
-# def reverse(a)
-#     a=a.to_s.reverse.to_i
-#     p a
+# the reverse without the
 
-# end
-# reverse(123)
+def reverse(a)
+   a = a.to_s.split("")
+   b=[]
+  for i in (a.length-1).downto(0)
+    b << a [i]
+  end
+  b.join("").to_i
 
+end
+p reverse(123)
 
+# find the sum of all element in the array
 
-# # the reverse without the 
+def add(a)
+  b = 0
+  for i in 0...a.length
+    b += a[i]
+  end
+  return b
+end
 
-# def reverse(a)
-#    a = a.to_s.split("")
-#    b=[]
-#   for i in (a.length-1).downto(0) 
-#     b << a [i]
-#   end
-#   b.join("").to_i
-  
-# end
-# p reverse(123)
-
-
-
+p add([2, 4, 6, 8, 10])
