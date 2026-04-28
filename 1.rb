57c99c3a883fafd5ace878beb39fec10963ec8c9
kichu70 <kiichu7034@gmail.com>
2026-04-28 19:19:44 +0530

    first commit

---
+++ 1.rb
@@ -0,0 +1,26 @@
+
+
+# def reverse(a)
+#     a=a.to_s.reverse.to_i
+#     p a
+
+# end
+# reverse(123)
+
+
+
+# # the reverse without the 
+
+# def reverse(a)
+#    a = a.to_s.split("")
+#    b=[]
+#   for i in (a.length-1).downto(0) 
+#     b << a [i]
+#   end
+#   b.join("").to_i
+  
+# end
+# p reverse(123)
+
+
+
